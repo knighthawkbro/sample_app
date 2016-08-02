55da6124ec79900e54e15bba1f9f268bad280e5c
Brady Walsh <walshb8@wit.edu>
2016-08-02 11:40:43 -0400

    Finish static pages

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,22 @@
 class StaticPagesController < ApplicationController
+  
   def home
+
   end
 
   def help
   end
+
+  def about
+  end
+
+  def contact
+  end
+
+  private
+
+  def setup
+    @base_title = "Ruby on Rails Tutorial Sample App"
+  end
+
 end
